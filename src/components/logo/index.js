e490fbf0456c131d9259631e29a282325033b4e7
6Ro0T <ranjithchandran220@gmail.com>
2024-04-01 12:11:02 +0530

    Repo created

---
+++ src/components/logo/index.js
@@ -0,0 +1,16 @@
+import Link from "next/link";
+import PropTypes from "prop-types";
+
+const Logo = ({src}) => {
+    return (
+        <Link href="/" className="tt-logo tt-logo-alignment">
+            <img src={src} alt="wokiee"/>
+        </Link>
+    );
+};
+
+Logo.propTypes = {
+    src: PropTypes.string.isRequired
+}
+
+export default Logo;
