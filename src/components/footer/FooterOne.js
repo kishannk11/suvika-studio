a8d6196b9a2ac029fd3f8c4e6f148e31400e31eb
6Ro0T <ranjithchandran220@gmail.com>
2024-06-04 10:24:55 +0530

    removed .next

--- src/components/footer/FooterOne.js
+++ src/components/footer/FooterOne.js
@@ -1,14 +1,14 @@
 import PropTypes from "prop-types";
 import {WidgetsOne as Widgets} from "./elements/widgets";
 import {NewsletterOne as Newsletter} from "@components/newsletter";
-import {CopyrightOne as Copyright} from "@components/footer/elements/copyright";
+// import {CopyrightOne as Copyright} from "@components/footer/elements/copyright";
 
 const FooterOne = ({className, newsletter, logo, dark}) => {
     return (
         <footer id="tt-footer" className={className ? className : ''}>
             {newsletter && <Newsletter />}
             <Widgets dark={dark}/>
-            <Copyright logo={logo} dark={dark}/>
+            {/* <Copyright logo={logo} dark={dark}/> */}
         </footer>
     );
 };
